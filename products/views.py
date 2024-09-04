1a0faffdb72e9df863fe5e720eb6f0f1e9d695f8
Warren Smith <warren.lee.smith@hotmail.co.uk>
2024-09-04 21:31:16 +0000

    ADD: Sorting and product counts to product page

--- products/views.py
+++ products/views.py
@@ -5,7 +5,6 @@ from .models import Product, Category
 
 # Create your views here.
 
-
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
@@ -16,20 +15,20 @@ def all_products(request):
     direction = None
 
     if request.GET:
-
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-            
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
