6b6226d939664e16dcfddae21458e0ae5ccd16e3
Warren Smith <warren.lee.smith@hotmail.co.uk>
2024-09-26 13:37:08 +0000

    ADD: stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Q3EBiRxoa5wCjJhNY3eAdIY7epiGUlMFqo8tHTZdeIGmZL9nH1Xv4yToWq1gqxtXkuCWo75xL010knXM7s0Wujt002t9xQqF6',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
